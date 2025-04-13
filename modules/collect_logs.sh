3be6e537594ca3a299541128667b96c9b9c95ff8
Ruslan <ruslan.akm@gmail.com>
2025-04-13 18:36:48 +0300

    Fix collect_logs

--- modules/collect_logs.sh
+++ modules/collect_logs.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-
+USER=`whoami`
 SCRIPT_DIR=$(dirname "$0")
 date=`date +%d-%m-%Y`
 
 journalctl --since "today" --no-pager > /var/log/daily_log_report_$date.txt
 grep -E -v '(usb|UFW|mtp-probe|rtkit|baloo|NetworkManager|dbus-daemon|telegram)' /var/log/daily_log_report_$date.txt > $SCRIPT_DIR/./../data/daily_log_report.txt
-chown ruslan:ruslan $SCRIPT_DIR/./../data/daily_log_report.txt
+chown $USER:$USER $SCRIPT_DIR/./../data/daily_log_report.txt
 
 
 # test run
