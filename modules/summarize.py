c136af85000bd73c8b951eb39f337f7f23f4583b
Ruslan <ruslan.akm@gmail.com>
2025-04-15 12:56:01 +0300

    ngpu value

--- modules/summarize.py
+++ modules/summarize.py
@@ -34,8 +34,8 @@ def summarisation_report():
 
     llm = Llama(
         model_path="/home/ruslan/.cache/lm-studio/models/bartowski/google_gemma-3-12b-it-GGUF/google_gemma-3-12b-it-Q4_K_S.gguf",
-        # n_gpu_layers=N_GPU, 
-        gpu_layers=N_GPU, 
+        n_gpu_layers=N_GPU, 
+        # gpu_layers=N_GPU, 
         # seed=1337, 
         n_ctx=17500, 
         use_mmap=True,
