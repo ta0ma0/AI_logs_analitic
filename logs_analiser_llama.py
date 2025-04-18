2d32b98fb3eac8cbb7cc02096c9dcb910fc7bdb2
Ruslan <ruslan.akm@gmail.com>
2025-04-18 09:32:43 +0300

    fix n_cnt

--- logs_analiser_llama.py
+++ logs_analiser_llama.py
@@ -87,7 +87,7 @@ def log_analizator(chunk):
       model_path="/home/ruslan/.cache/lm-studio/models/bartowski/google_gemma-3-12b-it-GGUF/google_gemma-3-12b-it-Q4_K_S.gguf",
       n_gpu_layers=int(N_GPU),
       # seed=1337, 
-      n_ctx=14000, 
+      n_ctx=15000, 
       use_mmap=False,
       verbose=False, # llama_cpp debug out (quiet)
     )
